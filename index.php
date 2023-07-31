acef11d83f50a7025a86ebc215a970b26da31ef6
sanirashuchi <2021751@iub.edu.bd>
2023-07-31 21:24:41 +0600

    first commit

---
+++ index.php
@@ -0,0 +1,27 @@
+
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Home</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+<body>
+
+<div class="header">
+	<h2>SUCCESSFULY LOGEDIN</h2><br>
+   
+	<p>
+  		 <a href="login.php">logout</a>
+  	</p>
+    
+</div>
+
+	
+
+
+    
+
+</body>
+</html>
---
+++ register.php
@@ -0,0 +1,48 @@
+<?php include('server.php') ?>
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Registration system PHP and MySQL</title>
+  <link rel="stylesheet" type="text/css" href="style.css">
+  
+</head>
+<body>
+<p class="bb">Welcome to my Assignment3-web page	</p>
+  <div class="header">
+  	<h2>Register Yourself With Send Us A Message</h2>
+  </div>
+
+  <form method="post" action="register.php">
+  	<?php include('errors.php'); ?>
+  	<div class="input-group">
+  	  <label>Username</label>
+  	  <input type="text" name="username" value="<?php echo $username; ?>">
+  	</div>
+  	<div class="input-group">
+  	  <label>Email</label>
+  	  <input type="email" name="email" value="<?php echo $email; ?>">
+  	</div>
+  	<div class="input-group">
+  	  <label>Password</label>
+  	  <input type="password" name="password_1">
+  	</div>
+  	<div class="input-group">
+  	  <label>Confirm password</label>
+  	  <input type="password" name="password_2">
+  	</div>
+
+      <div class="input-group">
+  	  <label>Send Us A Message</label>
+  	  <input type="text" name="Message" value="<?php echo $Message; ?>">
+  	</div>
+
+
+  	<div class="input-group">
+  	  <button type="submit" class="btn" name="reg_user">Register</button>
+  	</div>
+  	<p>
+  		 <a href="login.php">Sign in</a>
+  	</p>
+  </form>
+</body>
+</html>
